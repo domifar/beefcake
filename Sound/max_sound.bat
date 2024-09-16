0b3039db78ceb50a07e65f452a44d079d9ff9d23
domifar <dominik.faritsch@sbg.at>
2024-09-16 23:41:27 +0200

    fertig sound

--- Sound/max_sound.bat
+++ Sound/max_sound.bat
@@ -1,9 +1,4 @@
 @echo off
-rem Stelle sicher, dass der Pfad zu nircmd korrekt ist
-set nircmdPath=C:\Tools\nircmd\nircmd.exe
-
-rem Entmute das System
+set nircmdPath=C:\nircmd\nircmd.exe
 %nircmdPath% mutesysvolume 0
-
-rem Stelle die Lautstärke auf 100%
 %nircmdPath% setsysvolume 65535
--- Sound/start.bat
+++ Sound/start.bat
@@ -1,2 +1,4 @@
+@echo off
+timeout /t 3600 /nobreak
 start max_sound.bat
 start sound.mp3
