052c2ce053c0720af8465c1ef730c82b0d8d6626
domifar <dominik.faritsch@sbg.at>
2024-09-18 07:42:39 +0200

    sound mit bild

--- Sound/start.bat
+++ Sound/start.bat
@@ -2,3 +2,8 @@
 timeout /t 1800 /nobreak
 start max_sound.bat
 start startsound.bat
+start beefei_pose2.jpg
+start beefei_pose2.jpg
+start beefei_pose2.jpg
+start beefei_pose2.jpg
+start beefei_pose2.jpg
